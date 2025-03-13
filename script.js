9a28edcde2f71ae48c3bc04a6351b79621b333d0
Bafi Hoang <200473198+bafi-hng@users.noreply.github.com>
2025-03-13 14:31:12 +0100

    Enable basic calculations with reset after "="
    Still misses previous result => next firstNum aspect

--- script.js
+++ script.js
@@ -15,32 +15,68 @@ function divide(num1, num2) {
 }
 
 
-let num1;
-let num2;
-let operator;
-
-
 function operate(num1, num2, operator) {
     return operator(num1, num2);
 }
 
-
+// display
 const displayNum = document.querySelector(".display");
 
-let inputNum = "";
+
+// digit buttons
+let currNum = "";
+
+let firstNum = currNum;
+let secondNum;
+let operator;
 
 const digitButtons = document.querySelectorAll(".digit");
 digitButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        inputNum += button.textContent;
-        inputNum = parseInt(inputNum)
-        displayNum.textContent = (inputNum.toString());
+        currNum += button.textContent;
+        currNum = parseInt(currNum)
+        displayNum.textContent = (currNum.toString());
     })
+});
+
+// operator buttons
+const opButtons = document.querySelectorAll(".operator");
+opButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        firstNum = currNum;
+        currNum = "";
+        switch (button.textContent) {
+            case "+": 
+                operator = add;
+                break;
+            case "-": 
+                operator = subtract;
+                break;
+            case "*":
+                operator = multiply;
+                break;
+            case "/":
+                operator = divide;
+                break;
+        }
+    });
+});
+
+// result button
+const resButton = document.querySelector(".result");
+resButton.addEventListener("click", () => {
+    secondNum = currNum;
+    result = operator(firstNum, secondNum);
+    console.log(result);
+    displayNum.textContent = result.toString();
+    currNum = "";
 })
 
+
+// clear button
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener("click", () => {
-    inputNum = "";
+    currNum = "";
     displayNum.textContent = "0";
 })
 
