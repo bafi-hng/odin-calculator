5edc165a2ecf4e563895e351b832fe7099a3cbce
Bafi Hoang <200473198+bafi-hng@users.noreply.github.com>
2025-03-13 13:21:52 +0100

    Add display ability of digits buttons

--- script.js
+++ script.js
@@ -15,12 +15,26 @@ function divide(num1, num2) {
 }
 
 
-const num1;
-const num2;
-const operator;
+let num1;
+let num2;
+let operator;
 
 
 function operate(num1, num2, operator) {
-    return operator(num1, num2)
+    return operator(num1, num2);
 }
 
+
+const displayNum = document.querySelector(".display");
+
+let inputNum = "";
+
+const digitButtons = document.querySelectorAll(".digit");
+digitButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        inputNum += button.textContent;
+        displayNum.textContent = (parseInt(inputNum).toString());
+    })
+})
+
+console.log(displayNum.textContent)
